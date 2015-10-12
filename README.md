253255ca2e39c73b638d27513d09f1141a618fc9
pfwangs <calebwang@berkeley.edu>
2015-10-11 20:49:37 -0700

    Website

--- README.md
+++
@@ -1,1 +0,0 @@
-# pfwangs.github.io
--- actions.js
+++
@@ -1,6 +0,0 @@
-$(document).keypress(function(event) {
-    
-    if (event.which === 13) {
-        window.location = "file:///C:/Users/Caleb/Documents/Nuts/nuts.html";
-    }
-})
